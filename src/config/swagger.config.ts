80f043b15d1650cc610a42caaa58b3890ea4c868
Luiz <luiz_higuti@hotmai.com>
2025-03-02 18:53:25 -0300

    fix swagger on vercel

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -4,7 +4,20 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 export class SwaggerConfiguration {
   static for(app: NestFastifyApplication) {
     const document = SwaggerModule.createDocument(app, swaggerConfig);
-    SwaggerModule.setup('swagger', app, document);
+    SwaggerModule.setup('swagger', app, document, {
+      customSiteTitle: 'Api Docs',
+      customfavIcon:
+        'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+      customJs: [
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+      ],
+      customCssUrl: [
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+      ],
+    });
   }
 }
 
